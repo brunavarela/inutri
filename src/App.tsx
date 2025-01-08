16e7d509cc8be05c6f1be9ce09b4cac78bf9340b
Bruna Varela <brunavarela1@hotmail.com>
2025-01-08 17:16:14 -0300

    feat: install styled-components/types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from "react-native"
+import styled from "styled-components/native";
+
+const TextNew = styled.Text`
+  height: 100;
+`
 
 const App = () => {
   return (
     <View>
       <Text>Teste</Text>
+      <TextNew>Styled components</TextNew>
     </View>
   )
 }
