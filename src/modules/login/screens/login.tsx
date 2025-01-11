071a962fc934292414f0fafd3971eb76c976098a
Bruna Varela <brunavarela1@hotmail.com>
2025-01-11 12:51:03 -0300

    feat: insert background lp

--- src/modules/login/screens/login.tsx
+++ src/modules/login/screens/login.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native"
+import { Image, Text, TextInput, View } from "react-native"
+import { ContainerLogin, ImageBG } from "../styles/login.style";
 
 const Login = () => {
   return (
     <View>
-      <Text>Login</Text>
+      <ContainerLogin>
+        <ImageBG resizeMode="" source={require('../../../assets/images/background.png')}>
+          <Text>Login</Text>
+          <TextInput/>
+        </ImageBG>
+      </ContainerLogin>
     </View>
   )
 }
---
+++ src/modules/login/styles/login.style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const ContainerLogin = styled.View`
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`
+
+export const ImageBG = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+`
